c7646cd83b870456e6f9426786c8beb69ae41c91
dthrasher90 <dthrasher90@GMAIL.COM>
2017-04-25 15:39:39 -0800

    more ui states added
    added

--- public/src/controller/gamecontroller.js
+++ public/src/controller/gamecontroller.js
@@ -3,38 +3,85 @@ var myapp = angular.module('myApp', ['ui.router']);
 
 myapp.config(function($stateProvider) {
 
-  var helloState = {
-   name: 'hello',
-   url: '/hello',
-   templateUrl :'src/partials/rosters.html'
+  var playbookState = {
+   name: 'playbook',
+   url: '/playbook',
+   templateUrl :'src/partials/playbook.html'
  }
 
 
- var aboutState = {
-   name: 'about',
-   url: '/about',
-   template: '<h1> about </h1>'
+ var coachState = {
+   name: 'coach',
+   url: '/coach',
+   templateUrl: 'src/partials/coach.html'
  }
 
 
-  var yoState = {
-    name: 'yo',
-    url: '/yo',
-    template: '<h1> YO! </h1>'
+  var rosterState = {
+    name: 'depthchart',
+    url: '/depthchart',
+    templateUrl: 'src/partials/depthchart.html'
   }
 
- $stateProvider.state(helloState);
- $stateProvider.state(aboutState);
- $stateProvider.state(yoState);
+  var gamestatState = {
+    name: 'gamestat',
+    url: '/gamestat',
+    templateUrl: 'src/partials/gamestats.html'
+  }
+
+ $stateProvider.state(playbookState);
+ $stateProvider.state(coachState);
+ $stateProvider.state(rosterState);
+ $stateProvider.state(gamestatState);
 
 
 //end brackets
 });
 
-myapp.controller('myCtrl', function ($scope){
+myapp.controller('myCtrl', ['$scope', function ($scope){
 
 
-});
+
+
+}]);
+
+
+myapp.controller('rosterCtrl', ['$scope', function ($scope){
+
+  $scope.players = [
+    {
+      name : "F. Dixon",
+      position : "QB1",
+      stamina : "98%"
+    },
+    {
+      name : "B. Morgan",
+      position : "RB1",
+      stamina : "74%"
+    },
+    {
+      name : "C. Matthews",
+      position : "WR1",
+      stamina : "45%"
+    },
+    {
+      name : "A. Dobson",
+      position : "WR2",
+      stamina : "74%"
+    }
+
+  ];
+
+}]);
+
+
+
+
+myapp.controller('gamestatsCtrl', ['$scope', function ($scope){
+
+
+
+}]);
 
 
 // end controller bracket
